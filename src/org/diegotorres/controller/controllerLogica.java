bf92f4bba6117d00ef24e8ab5ec3ef3470171dba
Diego Torres <dtorres-2026036@kinal.edu.gt>
2026-04-12 18:18:44 -0600

    004 se creo el menu de opciones de logico y el primer verificarEdad

--- src/org/diegotorres/controller/controllerLogica.java
+++ src/org/diegotorres/controller/controllerLogica.java
@@ -4,10 +4,39 @@
  */
 package org.diegotorres.controller;
 
+
 /**
  *
  * @author informatica
  */
-public class controllerLogica {
-    
-}
+import java.util.Scanner;
+
+public class ControllerLogica {
+
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // El primer valor indica qué ejercicio ejecutar (21-30)
+        if (!sc.hasNextInt()) return;
+        int opcion = sc.nextInt();
+        
+switch (opcion) {
+            case 21: verificarEdad(sc.nextInt()); break;
+            case 22: esPar(sc.nextInt()); break;
+            case 23: controlSemaforo(sc.next()); break;
+            case 24: estaEnRango(sc.nextDouble(), sc.nextDouble(), sc.nextDouble()); break;
+            case 25: login(sc.next(), sc.next()); break;
+            case 26: obtenerMayor(sc.nextDouble(), sc.nextDouble()); break;
+            case 27: esBisiesto(sc.nextInt()); break;
+            case 28: estaAprobado(sc.nextDouble()); break;
+            case 29: esTriangulo(sc.nextDouble(), sc.nextDouble(), sc.nextDouble()); break;
+            case 30: compararCadenas(sc.next(), sc.next()); break;
+        }
+        sc.close();
+    }
+
+//              --- MÉTODOS ---
+
+    public static void verificarEdad(int edad) {
+        System.out.println(edad >= 18);
+    }
