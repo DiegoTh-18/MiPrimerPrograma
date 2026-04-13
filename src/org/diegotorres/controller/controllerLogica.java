468ac83389fc274cec0722855ed7e56f33115253
Diego Torres <dtorres-2026036@kinal.edu.gt>
2026-04-12 19:30:34 -0600

    009 se agregaron cambios no guardados

--- src/org/diegotorres/controller/controllerLogica.java
+++ src/org/diegotorres/controller/controllerLogica.java
@@ -40,6 +40,7 @@ switch (opcion) {
     public static void verificarEdad(int edad) {
         System.out.println(edad >= 18);
     }
+    
     public static void esPar(int numero) {
         System.out.println(numero % 2 == 0);
     }
@@ -49,8 +50,33 @@ switch (opcion) {
         else if (c.equals("amarillo")) System.out.println("Precaución");
         else if (c.equals("rojo")) System.out.println("Detenerse");
     }
+    
     public static void estaEnRango(double num, double min, double max){
         System.out.println(num >= min && num <= max);
         
     }
     
+    public static void login(String user, String pass) {
+        System.out.println(user.equals("admin") && pass.equals("1234"));
+    }
+    
+public static void obtenerMayor(double n1, double n2) {
+        System.out.println(Math.max(n1, n2));
+ }
+
+public static void esBisiesto(int anio) {
+        System.out.println((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0));
+    }
+
+    public static void estaAprobado(double nota) {
+        System.out.println(nota >= 60);
+    }
+    
+public static void esTriangulo(double a, double b, double c) {
+        System.out.println((a + b > c) && (a + c > b) && (b + c > a));
+    }
+
+    public static void compararCadenas(String s1, String s2) {
+        System.out.println(s1.equals(s2));
+    }
+}
